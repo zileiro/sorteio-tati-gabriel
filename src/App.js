d1d539bdbc54eb5eb7a33b7183eaf7ad83741db2
Vitória <mvmc@discente.ifpe.edu.br>
2022-12-13 15:48:49 -0300

    Adicionando titulo o botao, fazendo com que o gerador funcione!

--- src/App.js
+++ src/App.js
@@ -10,5 +10,15 @@ function randomNumber(min, max) {
 const handleClick = () => {
   setNum(randomNumber(1, 100));
 }
+return (
+  <div className="container-home">
+  <div className="title">
+    <h1>Gerador de números aleatórios</h1>
+      </div>
+      <h2>Número Gerado : {num}</h2>
+      <button onClick={handleClick}>Realizar Sorteio</button>
+      </div>
+      );
+    }
 
 export default App;
