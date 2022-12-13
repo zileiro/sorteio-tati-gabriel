73010cf0abc47da594cb1d238b33f80ea01f77f9
Vitória <mvmc@discente.ifpe.edu.br>
2022-12-13 15:47:57 -0300

    Importando o useState, adicionando a funcao randomNumber!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [num, setNum] = useState(0);
+
+function randomNumber(min, max) {
+  return Math.round(Math.random() * (max - min + 1)) + min;
+}
+const handleClick = () => {
+  setNum(randomNumber(1, 100));
 }
 
 export default App;
